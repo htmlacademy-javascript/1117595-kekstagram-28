35c2f289ddddfdfa9d2abd43df9f3d807cd1f6b0
Dmitry <lysogordmitry@gmail.com>
2023-03-20 20:59:30 +0300

    дз 7й раздел готово для проверки

---
+++ js/thumbnails-rendering.js
@@ -0,0 +1,24 @@
+import { createPhotos } from './data.js';
+
+const pictureContainer = document.querySelector('.picture');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const picturesFragment = document.createDocumentFragment();
+
+const renderThumbnails = createPhotos();
+
+renderThumbnails.forEach((photo) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = photo.url;
+  pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photo.comment.length;
+
+  picturesFragment.append(pictureElement);
+});
+
+pictureContainer.append(picturesFragment);
+
+export { renderThumbnails };
