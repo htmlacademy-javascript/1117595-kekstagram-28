9f0b123ebbde0690ce82207f9611db9407fa2084
hoqus <lysogordmitry@gmail.com>
2023-04-07 17:50:43 +0300

    дз 11й раздел на проверку

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 import { renderThumbnails } from './thumbnails-rendering.js';
 import { renderGallery } from './gallery.js';
-import { createPhotos } from './data.js';
+// import { createPhotos } from './data.js';
 import './form.js';
 import './scale.js';
 import './slider.js';
+import { showErrorText } from './message.js';
+import { getData } from './api.js';
 
-const photos = createPhotos();
-renderThumbnails(photos);
-renderGallery(photos);
+
+getData()
+  .then((data) => {
+    renderThumbnails(data);
+    renderGallery(data);
+  })
+  .catch(() => {
+    showErrorText();
+  }
+  );
+
+// const photos = createPhotos();
+// renderThumbnails(photos);
+// renderGallery(data);
--- js/thumbnails-rendering.js
+++ js/thumbnails-rendering.js
@@ -1,10 +1,10 @@
 const pictureContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const createElement = ({url, comment, likes, id}) => {
+const createElement = ({url, comments, likes, id}) => {
   const pictureClone = pictureTemplate.cloneNode(true);
   pictureClone.querySelector('.picture__img').src = url;
-  pictureClone.querySelector('.picture__comments').textContent = comment.length;
+  pictureClone.querySelector('.picture__comments').textContent = comments.length;
   pictureClone.querySelector('.picture__likes').textContent = likes;
   pictureClone.querySelector('.picture__img').dataset.pictureCloneId = id;
   return pictureClone;
