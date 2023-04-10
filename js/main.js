b47821ea20fe3bc2a9e0328b9e8945a4f97be1d5
hoqus <lysogordmitry@gmail.com>
2023-04-10 17:33:41 +0300

    12й раздел 2я часть тоже готова к проверке

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import './form.js';
 import './scale.js';
 import './slider.js';
+import './upload-picture.js';
 import { renderThumbnails } from './thumbnails-rendering.js';
 import { renderGallery } from './gallery.js';
 import { sortPhoto } from './filter.js';
